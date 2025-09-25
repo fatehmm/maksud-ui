adbd17d242d639f1e04a02286949fec10d0f0cb2
Fateh Mammadli <fmammadli23@ku.edu.tr>
2025-09-25 18:52:26 +0400

    Update biome configuration, add new dependencies, enhance MDX components, modify global styles, and replace logo in layout.

--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,3 +1,5 @@
+import { CodeBlock, Pre } from 'fumadocs-ui/components/codeblock';
+import * as TabsComponents from 'fumadocs-ui/components/tabs';
 import defaultMdxComponents from 'fumadocs-ui/mdx';
 import type { MDXComponents } from 'mdx/types';
 
@@ -5,6 +7,12 @@ import type { MDXComponents } from 'mdx/types';
 export function getMDXComponents(components?: MDXComponents): MDXComponents {
   return {
     ...defaultMdxComponents,
+    ...TabsComponents,
+    pre: ({ ref: _ref, ...props }) => (
+      <CodeBlock {...props}>
+        <Pre>{props.children}</Pre>
+      </CodeBlock>
+    ),
     ...components,
   };
 }
