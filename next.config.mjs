294271c6b0d6a0c9b96591d1d247d3c545ae9631
Fateh Mammadli <fmammadli23@ku.edu.tr>
2025-09-26 20:55:15 +0400

    Add redirects in next.config.mjs for improved routing to JSON endpoints and default styles

--- next.config.mjs
+++ next.config.mjs
@@ -5,6 +5,35 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      /**
+       * @see https://github.com/magicuidesign/magicui/blob/main/next.config.mjs
+       */
+      {
+        source: '/r/index',
+        destination: '/r/index.json',
+        permanent: true,
+      },
+      {
+        source: '/r/registry',
+        destination: '/r/registry.json',
+        permanent: true,
+      },
+      {
+        source: '/r/:name((?!index\\.json|registry\\.json|styles/).*)',
+        destination: '/r/styles/default/:name.json',
+        permanent: true,
+        missing: [
+          {
+            type: 'query',
+            key: '_redirected',
+            value: undefined,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withMDX(config);
